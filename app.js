9d18e744b53fa9d85d501a307b28f2f419812485
Osama Sayed <oosooweezy@gmail.com>
2023-01-28 15:32:45 +0200

    Osama branch

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/Osama', (req, res) => {
+  res.send('Osama branch')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
